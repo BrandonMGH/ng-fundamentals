b57c524d23a20c9baa70a9354ec07bb6b8954f3f
BrandonMGH <BrandonMGH@Yahoo.com>
2020-07-06 18:04:58 -0700

    Halfway through Testing Mock Calls.  Still cannot figure out issue with first test

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -3,21 +3,32 @@ import { ISession } from '../shared/event.model';
 import { of, Observable } from 'rxjs';
 
 describe('VoterService', () => {
-    let voterService: VoterService;
-    let mockHttp;
+    let voterService: VoterService,
+     mockHttp;
     beforeEach(() => {
         mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post']);
         voterService = new VoterService(mockHttp);
+
     });
 
     describe('deleteVoter', () => {
         it('should remove the voter from the list of voters', () => {
             let session = { id: 6, voters: ['joe', 'john']};
             mockHttp.delete.and.returnValue(of(false));
-            voterService.deleteVoter(6, session as ISession, 'joe');
+
+            voterService.deleteVoter(3, session as ISession, 'joe');
 
             expect(session.voters.length).toBe(2);
             expect(session.voters[0]).toBe('john');
         });
+
+        it('should call http.delete with the right URL', () => {
+            let session = { id: 6, voters: ['joe', 'john']};
+            mockHttp.delete.and.returnValue(of(false));
+
+            voterService.deleteVoter(3, session as ISession, 'joe');
+
+            expect(mockHttp.delete).toHaveBeenCalledWith(`/api/events/3/sessions/6/voters/joe`);
+        })
     });
 })
